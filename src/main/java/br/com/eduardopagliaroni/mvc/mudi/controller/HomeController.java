3203db48cd3c309bf0dc38cc117c9e22c43279b8
Eduardo Pagliaroni <dudupagliaroni@gmail.com>
2021-10-14 11:05:26 -0300

    criando o repository

---
+++ src/main/java/br/com/eduardopagliaroni/mvc/mudi/controller/HomeController.java
@@ -0,0 +1,26 @@
+package br.com.eduardopagliaroni.mvc.mudi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.eduardopagliaroni.mvc.mudi.model.Pedido;
+import br.com.eduardopagliaroni.mvc.mudi.repository.PedidoRepository;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@GetMapping("/home")
+	public String home(Model model) {
+		List<Pedido> pedidos = pedidoRepository.recuperaTodosOsPedidos();
+		model.addAttribute("pedidos", pedidos);
+		return "home";
+	}
+
+}
