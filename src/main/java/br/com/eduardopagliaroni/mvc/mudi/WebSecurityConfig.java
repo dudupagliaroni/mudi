f045940fac39bba08a0e913c0cbc887ad232affe
Eduardo Pagliaroni <dudupagliaroni@gmail.com>
2021-10-19 15:40:48 -0300

    implementando o logout

--- src/main/java/br/com/eduardopagliaroni/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/eduardopagliaroni/mvc/mudi/WebSecurityConfig.java
@@ -20,7 +20,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.anyRequest().authenticated()
 		.and()
-		.formLogin(form -> form.loginPage("/login").permitAll());
+		.formLogin(form -> form.loginPage("/login").permitAll())
+		.logout(logout -> logout.logoutUrl("/logout"));
 
 	}
 
